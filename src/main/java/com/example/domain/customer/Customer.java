da5afd2c67922a2972eddc69ebfdadb16a3f9005
Rodrigo Faria <falico@gmail.com>
2016-07-28 03:05:08 +0000

    one step more

--- src/main/java/com/example/domain/customer/Customer.java
+++ src/main/java/com/example/domain/customer/Customer.java
@@ -1,25 +1,19 @@
 package com.example.domain.customer;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 
+import lombok.Data;
+
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Customer {
-    @Id
-    private String id;
+	@Id
+	@GeneratedValue
+	private Long id;
 
     private String firstName;
     private String lastName;
-
-    public Customer() {}
-
-    public Customer(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "Customer[id=%s, firstName='%s', lastName='%s']",
-                id, firstName, lastName);
-    }
 }
